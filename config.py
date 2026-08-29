284eb34e5d73482a024daee2fc5d4c869b859fcf
melkymanipis82-sys <melkymanipis82@gmail.com>
2026-08-30 00:10:39 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# Telegram Bot configuration
+# IMPORTANT: This file contains a bot credential. Keep it private.
+BOT_TOKEN = "8657858646:AAGW5uCUbi1jvPgmpYr2b0dP3gbAesQnnuk"
+ALLOWED_CHAT_ID = 6656858850
