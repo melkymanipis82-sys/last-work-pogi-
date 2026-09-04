ce2ed22860991112fd63d08dbf5fe5b910c6b52e
melkymanipis82-sys <melkymanipis82@gmail.com>
2026-09-04 17:39:26 +0800

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Telegram Bot configuration
 # IMPORTANT: This file contains a bot credential. Keep it private.
-BOT_TOKEN = "8657858646:AAGW5uCUbi1jvPgmpYr2b0dP3gbAesQnnuk"
+BOT_TOKEN = "8726994993:AAHmtAv6pGUS8rrvMuqHdSoI4umtLOdyWQ4"
 ALLOWED_CHAT_ID = 6656858850
